b2170feff2200125efc63ccfb2728f38e7b31fb8
acarlseen <110647552+acarlseen@users.noreply.github.com>
2024-02-22 12:43:42 -0800

    fix title for landing page

--- coffee/site/routes.py
+++ coffee/site/routes.py
@@ -11,5 +11,5 @@ site = Blueprint('site', __name__,
 @site.route('/')
 def home():
     return render_template('index.html',
-                           title='')
+                           title='Home')
 
