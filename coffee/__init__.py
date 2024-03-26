75c3bc0b034696ba62b66fcb1de2c6dbad8140b0
acarlseen <110647552+acarlseen@users.noreply.github.com>
2024-03-25 20:35:25 -0700

    added CORS

--- coffee/__init__.py
+++ coffee/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from config import Config
 
@@ -14,6 +15,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False, template_folder='./base/templates', static_folder='./base/static')
     app.config.from_object(Config)
 
+    CORS(app)
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
